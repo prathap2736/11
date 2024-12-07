fbd4a9844aa272dd3c70bc171e670b5b043a058c
PrathapCodes <prathap4357@gmail.com>
2024-12-07 11:41:02 -0800

    Add files via upload

---
+++ form popup/index.js
@@ -0,0 +1,9 @@
+// Open modal when "Sign Up" button is clicked
+document.getElementById("open").addEventListener("click", function() {
+    document.getElementById("model").style.display = "block";
+});
+
+// Close modal when "x" button is clicked
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("model").style.display = "none";
+});
